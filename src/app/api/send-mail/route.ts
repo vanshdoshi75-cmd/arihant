9a89d39b57e5888c7bd4bbba0acca8765aec7b57
Vansh <vanshdoshi75@gmail.com>
2026-05-24 01:21:45 +0530

    ko

--- src/app/api/send-mail/route.ts
+++ src/app/api/send-mail/route.ts
@@ -13,7 +13,9 @@ export async function POST(req: Request) {
 
     const transporter =
       nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+port: 587,
+secure: false,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,

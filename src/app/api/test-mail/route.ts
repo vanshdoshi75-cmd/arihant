1f82b706b63c28cd079572cd7cb2e1f84ae920f6
Vansh <vanshdoshi75@gmail.com>
2026-05-25 19:21:42 +0530

    ew

--- src/app/api/test-mail/route.ts
+++ src/app/api/test-mail/route.ts
@@ -1,54 +1,35 @@
 import nodemailer from "nodemailer";
 
 export async function GET() {
-
   try {
-
-    const transporter =
-      nodemailer.createTransport({
-
-        service: "gmail",
-
-        auth: {
-
-          user:
-            process.env.EMAIL_USER,
-
-          pass:
-            process.env.EMAIL_PASS,
-
-        },
-
-      });
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
     await transporter.sendMail({
-
-      from:
-        process.env.EMAIL_USER,
-
-      to:
-        "manibhadra809@gmail.com",
-
-      subject:
-        "TEST MAIL",
-
-      text:
-        "MAIL WORKING",
-
+      from: process.env.EMAIL_USER,
+      to: "manibhadra809@gmail.com",
+      subject: "TEST MAIL",
+      text: "MAIL WORKING SUCCESSFULLY",
     });
 
     return Response.json({
       success: true,
+      message: "Mail sent",
     });
-
   } catch (error: any) {
-
-    console.log(error);
-
     return Response.json({
       success: false,
       error: error.message,
     });
-
   }
 }

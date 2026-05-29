e51273a300984c9a2223f8993494dddd1dcc8075
Vansh <vanshdoshi75@gmail.com>
2026-05-29 12:34:48 +0530

    ewr

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -428,7 +428,7 @@ Password: ${password}`
 try {
 
 const res = await fetch(
-`${window.location.origin}/api/send-mail`,
+`${window.location.origin}/api/create-user`,
 {
 method:"POST",
 headers:{
@@ -702,7 +702,7 @@ Password: ${password}`
 try {
 
   const mailRes = await fetch(
-    `${window.location.origin}/api/send-mail`,
+    `${window.location.origin}/api/create-user`,
     {
       method:"POST",
       headers:{
